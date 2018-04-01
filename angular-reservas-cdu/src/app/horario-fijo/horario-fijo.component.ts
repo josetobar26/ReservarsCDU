870dcf5fe22318a6538dab7d9e3835dbb3e834af
fbambague7 <36634463+fbambague7@users.noreply.github.com>
2018-04-01 14:25:28 -0500

    sdf
    asdas

--- angular-reservas-cdu/src/app/horario-fijo/horario-fijo.component.ts
+++ angular-reservas-cdu/src/app/horario-fijo/horario-fijo.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./horario-fijo.component.css']
 })
 export class HorarioFijoComponent implements OnInit {
-
+    
   constructor() { }
 
   ngOnInit() {
