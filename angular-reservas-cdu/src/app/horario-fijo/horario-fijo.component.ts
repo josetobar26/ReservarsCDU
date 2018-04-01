d6326f12e4644545978b3deb16b124928ae2f4ed
fbambague7 <36634463+fbambague7@users.noreply.github.com>
2018-04-01 15:20:29 -0500

    horarioFijo

--- angular-reservas-cdu/src/app/horario-fijo/horario-fijo.component.ts
+++ angular-reservas-cdu/src/app/horario-fijo/horario-fijo.component.ts
@@ -8,14 +8,9 @@ import { EspaciodeportivoService } from '../espaciodeportivo.service';
   styleUrls: ['./horario-fijo.component.css']
 })
 export class HorarioFijoComponent implements OnInit {
-<<<<<<< HEAD
-    
-  constructor() { }
-=======
 
   private deportes:Deporte[];
   constructor(private espacioService:EspaciodeportivoService) { }
->>>>>>> 2ee50a4bba173e9037ae74f6e31463a602c976fa
 
   ngOnInit() {
   	this.getDeportes();
