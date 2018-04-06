4074641f78ec9ee168d45a7d3638adc13fbba162
millercastillo <millercastillo@unicauca.edu.co>
2018-04-06 16:18:07 -0500

    mostrar foto

--- angular-reservas-cdu/src/app/app-routing.module.ts
+++ angular-reservas-cdu/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { EspacioDeportivosComponent } from './espacio-deportivos/espacio-deportivos.component';
 import { AgregarescenarioComponent } from './agregarescenario/agregarescenario.component';
 import { HorarioFijoComponent} from './horario-fijo/horario-fijo.component';
+import { MostrarFotoComponent} from './mostrar-foto/mostar-foto.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 const routes: Routes = [
   { path: '', redirectTo: '/espaciodeportivos', pathMatch: 'full' },	
   { path: 'espaciodeportivos', component: EspacioDeportivosComponent },
    { path: 'agregarescenario', component: AgregarescenarioComponent },
-   { path: 'horariofijo', component: HorarioFijoComponent }
+   { path: 'horariofijo', component: HorarioFijoComponent },
+   { path: 'mostrarfoto', component: MostrarFotoComponent }
    
 ];
 
